df3e7e79882e9376f71d7a8b4fd3a75353b1cfb9
rxfatalslash <aaronsanchezmenendez@gmail.com>
2023-01-10 17:59:46 +0100

    Remove

--- include/header.php
+++
@@ -1,6 +0,0 @@
-<?php
-    echo '<a href="/require.php">require</a> |
-          <a href="/include.php">include</a> |
-          <a href="require_once">require_once</a> |
-          <a href="include_once">include_once</a> |';
-?>
--- include/include.php
+++
@@ -1,4 +0,0 @@
-<?php
-    require 'header.php';
-    include 'menu.html';
-?>
