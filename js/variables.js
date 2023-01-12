26a3fd8561d0b6e30b8e85db3732153ee90ef86c
rxfatalslash <aaronsanchezmenendez@gmail.com>
2023-01-12 13:07:44 +0100

    Update variables.js

--- js/variables.js
+++ js/variables.js
@@ -10,8 +10,17 @@ var x;
 document.write("<table><thead><th>v1</th><th>v2</th><th>v3</th><th>empleado</th><th>paises</th><th>fecha</th><th>nada</th><th>x</th></thead><tbody><tr><td>" + typeof v1 + "</td><td>" + typeof v2 + "</td><td>" + typeof v3 + "</td><td>" + typeof empleado + "</td><td>" + typeof paises + "</td><td>" + typeof fecha + "</td><td>" + typeof nada + "</td><td>" + typeof x + "</td></tr></tbody></table>");
 
 var valores = [true, 5, false, "hola", "adios", 2];
+var long1 = valores[3].length;
+var long2 = valores[4].length;
 
-if ((valores[3] > valores[4])) {
+if (valores[3] > valores[4]) {
+    document.write(valores[3] + " es mayor que " + valores[4]);
+}
+else {
+    document.write(valores[4] + " es mayor que " + valores[3]);
+}
+
+if (long1 > long2) {
     document.write(valores[3] + " es mayor que " + valores[4]);
 }
 else {
@@ -19,3 +28,5 @@ else {
 }
 
 document.write("<br>");
+
+function str()
