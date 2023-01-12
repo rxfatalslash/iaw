2a6084ad927b347088e1b6a827544b64e7e958bb
rxfatalslash <aaronsanchezmenendez@gmail.com>
2023-01-12 13:35:21 +0100

    Update variables.js

--- js/variables.js
+++ js/variables.js
@@ -41,6 +41,48 @@ function contar() {
     document.write("Hay " + count + " cadenas de texto");
 }
 
+function contarTipos() {
+    var countStr = 0;
+    var countNmb = 0;
+    var countBool = 0;
+    var countObj = 0;
+    var countUnd = 0;
+    for (i in valores) {
+        if (typeof valores[i] == "string") {
+            countStr++;
+        }
+        else if (typeof valores[i] == "number") {
+            countNmb++;
+        }
+        else if (typeof valores[i] == "boolean") {
+            countBool++;
+        }
+        else if (typeof valores[i] == "object") {
+            countObj++;
+        }
+        else if (typeof valores[i] == "undefined") {
+            countUnd++;
+        }
+    }
+    document.write("Hay " + countStr + " strings, " + countNmb + " numbers, " + countBool + " booleans, " + countObj + " objects y " + countUnd + " undefineds");
+}
+
+function contarObjeto() {
+    var count = 0;
+    for (i in valores) {
+        count++;
+    }
+    document.write("Hay " + count + " elementos dentro del objeto");
+}
+
 var valores = [4, "pepe", true, 34, "que pasa con el ejercicio anterior?", false, 23, "nusé", 2, true]
 
 contar();
+
+document.write("<br>");
+
+contarObjeto();
+
+document.write("<br>");
+
+contarTipos();
