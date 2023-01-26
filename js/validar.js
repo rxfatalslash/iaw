fc9026cf865245532c8772e2346f71122943f635
rxfatalslash <aaronsanchezmenendez@gmail.com>
2023-01-26 19:47:34 +0100

    Update validar

--- js/validar.js
+++ js/validar.js
@@ -1,45 +1,38 @@
 function validar() {
-    var errores;
-
-    var name = document.forms["fdatos"]["nombre"].value;
-    if (name == null || name == "") {
-        errores = "Debes proporcionar un nombre";
-    }
-    
-    var password = document.forms["fdatos"]["clave"].value;
-    if (password == null || password == "") {
-        clave = errores + "<br>Debes proporcionar una contraseña";
+    // Nombre
+    let nombre = document.getElementsByName('nombre').value;
+    if (nombre == null || nombre == "") {
+        alert("Añade un nombre");
     }
 
-    var genre = document.forms["fdatos"]["genero"].value;
-    if (genre == null || genre == "") {
-        errores = errores + "<br>Debes seleccionar un género";
+    // Contraseña
+    let clave = document.getElementsByName('clave').value;
+    if (clave == null || clave == "") {
+        alert("Añade una contraseña");
     }
 
-    var birthDate = document.forms["fdatos"]["fechaNacimiento"];
-    if (birthDate == null || birthDate == "") {
-        errores = errores + "<br>Debes seleccionar una fecha de nacimiento";
+    // Género
+    let gen_selected = false;
+    let generos = document.getElementsByName('genero');
+    for (let genero of generos) {
+        if (genero.checked) {
+            gen_selected = true;
+        }
     }
 
-    var country = document.forms["fdatos"]["pais"].value;
-    if (country == null || country == "") {
-        errores = errores + "<br>Debes seleccionar un país"
+    if (!gen_selected) {
+        alert("Selecciona un género");
     }
 
-    var accept = document.forms["fdatos"]["acepto"].value;
-    if (accept == null || accept == "") {
-        errores = errores + "<br>Debes escoger si quieres publicidad";
+    // Fecha de nacimiento
+    let fecha = document.getElementsByName('fecha').value;
+    if (fecha == null || fecha == "") {
+        alert("Introduce una fecha");
     }
 
-    var comments = document.forms["fdatos"]["comentarios"].value;
-    if (comments == null || comments == "") {
-        errores = errores + "<br>No se han recibido comentarios";
+    // País
+    let pais = document.getElementsByName('pais').value;
+    if (pais == null || pais == "") {
+        alert("Elige un país");
     }
-
-    var file = document.forms["fdatos"]["foto"].value;
-    if (file == null || file == "") {
-        errores = errores + "<br>No se ha recibido ninguna foto";
-    }
-
-    alert(errores);
 }
